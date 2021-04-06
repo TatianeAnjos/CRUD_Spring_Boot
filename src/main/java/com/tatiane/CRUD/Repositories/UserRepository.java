d90ebbddd879368d1d3a6c33a8e3ee88e8b15d66
Tatiane Anjos <tatianeanjos1997@gmail.com>
2021-04-05 21:12:33 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/tatiane/CRUD/Repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tatiane.CRUD.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tatiane.CRUD.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
