42237ec8c5c338d007251f9364d21ff3f4d515f5
Tatiane Anjos <tatianeanjos1997@gmail.com>
2021-04-13 20:45:28 -0300

    Order, Instant, AssociacoesUserOrder

---
+++ src/main/java/com/tatiane/CRUD/Services/OrderServices.java
@@ -0,0 +1,26 @@
+package com.tatiane.CRUD.Services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tatiane.CRUD.Repositories.OrderRepository;
+import com.tatiane.CRUD.entities.Order;
+
+@Service
+public class OrderServices {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
